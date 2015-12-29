95136a77f7cb413303f2b10a6239962b923614af
Tadashi Hirata <heita@ip-172-31-27-172.ap-northeast-1.compute.internal>
2015-12-29 22:39:01 +0900

    Change run.sh path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
- ~/spark-1.5.2-bin-hadoop2.6/bin/spark-submit --class com.oreilly.learningsparkexamples.mini.scala.WordCount target/scala-2.10/learning-spark-mini-example_2.10-0.0.1.jar ../../spark-1.5.2-bin-hadoop2.6/README.md ~/wordcounts
+ ~/spark-1.5.2/bin/spark-submit --class com.oreilly.learningsparkexamples.mini.scala.WordCount target/scala-2.10/learning-spark-mini-example_2.10-0.0.1.jar ../../spark-1.5.2-bin-hadoop2.6/README.md ~/wordcounts
 
